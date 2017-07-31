d6ecb2f3b83ac20165fc37c858e94305fda236f7
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2017-07-31 22:54:20 +0200

    Util::parseCsvFileToObjects => UtilCsv::parseCsvFile

--- src/WhyooOs/Util/Util.php
+++ src/WhyooOs/Util/Util.php
@@ -203,31 +203,6 @@ class Util
 
 
 
-    /**
-     * used by FixturesUtil
-     *
-     * @param $pathCsv
-     * @return array
-     */
-    public static function parseCsvFileToObjects($pathCsv)
-    {
-        $fileHandle = fopen($pathCsv, 'r');
-        $arr = [];
-        while (($row = fgetcsv($fileHandle)) !== FALSE) {
-            $arr[] = $row;
-        }
-        fclose($fileHandle);
-
-        $headers = array_shift($arr); // remove header row
-        $aObjects = [];
-        foreach ($arr as $row) {
-            if (count($row) == count($headers)) { // valid row
-                $aObjects[] = (object)array_combine($headers, $row);
-            }
-        }
-
-        return $aObjects;
-    }
 
 
 }
