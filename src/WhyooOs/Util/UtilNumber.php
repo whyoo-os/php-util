9677dda15bd3819710cbfe02243352dad7d9d0c6
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2017-08-19 17:41:02 +0200

    UtilNumber::stringToNumber added

--- src/WhyooOs/Util/UtilNumber.php
+++ src/WhyooOs/Util/UtilNumber.php
@@ -10,15 +10,26 @@ namespace WhyooOs\Util;
 class UtilNumber
 {
     /**
-     * @param float/int $number
-     * @param float/int $min
-     * @param float/int $max
-     * @return float/int
+     * @param float|int $number
+     * @param float|int $min
+     * @param float|int $max
+     * @return float|int
      */
     public static function clip($number, $min, $max)
     {
         return max(min($number, $max), $min);
     }
 
+
+    /**
+     * replaces komma (german notation) with dot (american notation)
+     * eg "100,12" --> 100.12
+     * @param $string
+     * @return float
+     */
+    public static function stringToNumber($string)
+    {
+        return floatval(str_replace(',', '.', $string));
+    }
 }
 
