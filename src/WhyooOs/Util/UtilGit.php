e1a0083a13e686b8d91521b7ea583ab6e1208477
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2017-11-29 12:06:43 +0100

    UtilGit - added optional $path parameters

--- src/WhyooOs/Util/UtilGit.php
+++ src/WhyooOs/Util/UtilGit.php
@@ -12,16 +12,17 @@ class UtilGit
     /**
      * legacy
      */
-    public static function version()
+    public static function version(string $path=null)
     {
-        exec('git describe --always', $version_mini_hash);
-        exec('git rev-list HEAD | wc -l', $version_number);
-        exec('git log -1', $line);
+        $paramPath = $path ? " -C $path " : '';
+        exec("git $paramPath describe --always", $version_mini_hash);
+        exec("git $paramPath rev-list HEAD | wc -l", $version_number);
+        exec("git $paramPath log -1", $line);
         $version['short'] = "v1." . trim($version_number[0]) . "." . $version_mini_hash[0]; // ?? v1 ?? remove?
         $version['full'] = "v1." . trim($version_number[0]) . ".$version_mini_hash[0] (" . str_replace('commit ', '', $line[0]) . ")";; // ?? v1 ?? remove?
         // TODO: 2 functions for short and full hash (used for cache busting)
-        $version['shortHash'] = exec('git rev-parse --short HEAD');
-        $version['fullHash'] = exec('git rev-parse HEAD');
+        $version['shortHash'] = exec("git $paramPath rev-parse --short HEAD");
+        $version['fullHash'] = exec("git $paramPath rev-parse HEAD");
 
         return $version;
     }
@@ -29,9 +30,10 @@ class UtilGit
     /**
      * used in shipping for "version number"
      */
-    public static function getDate()
+    public static function getDate(string $path=null)
     {
-        return exec('git log -1 --format=%cd --date=short');
+        $paramPath = $path ? " -C $path " : '';
+        return exec("git $paramPath log -1 --format=%cd --date=short");
     }
 
     /**
@@ -39,9 +41,10 @@ class UtilGit
      * ideal for cache-buster
      * @return string
      */
-    public static function getShortHash()
+    public static function getShortHash(string $path=null)
     {
-        return exec('git rev-parse --short HEAD');
+        $paramPath = $path ? " -C $path " : '';
+        return exec("git $paramPath rev-parse --short HEAD");
     }
 }
 
