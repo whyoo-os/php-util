40bc673cdb56fcddeb9475648b05db29e98543b6
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2018-05-15 02:54:37 +0200

    image stuff

--- src/WhyooOs/HelperClasses/ImageTagger.php
+++ src/WhyooOs/HelperClasses/ImageTagger.php
@@ -2,6 +2,8 @@
 
 namespace WhyooOs\HelperClasses;
 
+use WhyooOs\Util\UtilAssert;
+use WhyooOs\Util\UtilDebug;
 use WhyooOs\Util\UtilImage;
 
 
@@ -11,67 +13,68 @@ use WhyooOs\Util\UtilImage;
 class ImageTagger
 {
 
-    const ALLOWED_POSITIONS = ['TL', 'TC', 'TR', 'ML', 'MC', 'MR', 'BL', 'BC', 'BR', 'AT', 'AB', 'AL', 'AR'];
+    const ALLOWED_POSITIONS = ['TL', 'TC', 'TR', 'ML', 'MC', 'MR', 'BL', 'BC', 'BR'/*, 'AT', 'AB', 'AL', 'AR'*/];
 
-    /**
-     * private helper
-     * append image at t, b, l or r
-     * only 100% scale possible
-     *
-     * @param $pathImage
-     * @param $pathTag
-     * @param $tagPosition
-     * @param $pathDest
-     */
-    private function _appendToImage($pathImage, $pathTag, $tagPosition, $pathDest)
-    {
-        $sizeSrc = getimagesize($pathImage);
-        $widthSrc = $sizeSrc[0];
-        $heightSrc = $sizeSrc[1];
-        $sizeTag = getimagesize($pathTag);
-        $widthTag = $sizeTag[0];
-        $heightTag = $sizeTag[1];
-
-        // load images
-        $imImage = UtilImage::loadImage($pathImage);
-        $imTag = UtilImage::loadImage($pathTag);
-
-
-        if ($tagPosition == "T" || $tagPosition == "B") {
-            $scaleTag = (float)$widthSrc / $widthTag;
-            // W/H of Dest
-            $newWidth = $widthSrc;
-            $newHeight = $heightSrc + $heightTag * $scaleTag;
-
-            $dstXImage = 0;
-            $dstYImage = $tagPosition == 'T' ? $heightTag * $scaleTag : 0;
-
-            $dstXTag = 0;
-            $dstYTag = $tagPosition == 'T' ? 0 : $heightSrc;
-        } else { // L or R
-            $scaleTag = (float)$heightSrc / $heightTag;
-            $newWidth = $widthSrc + $widthTag * $scaleTag;
-            $newHeight = $heightSrc;
-
-            $dstXImage = $tagPosition == 'L' ? $widthTag * $scaleTag : 0;
-            $dstYImage = 0;
-
-            $dstXTag = $tagPosition == 'L' ? 0 : $widthSrc;
-            $dstYTag = 0;
-        }
-
-        // create empty image
-        $imDest = imagecreatetruecolor($newWidth, $newHeight);
-#	imagealphablending( $imDest, true);
-#	imagesavealpha( $imDest, true);
-        imagefill($imDest, 0, 0, 0xff0000);
-
-        // copy image (not resized)
-        imagecopy($imDest, $imImage, $dstXImage, $dstYImage, 0, 0, $widthSrc, $heightSrc);
-        // copy tag (resampled)
-        imagecopyresampled($imDest, $imTag, $dstXTag, $dstYTag, 0, 0, $widthTag * $scaleTag, $heightTag * $scaleTag, $widthTag, $heightTag);
-        UtilImage::saveImage($imDest, $pathDest);
-    }
+//    /**
+//     * 05/2018 UNUSED
+//     * private helper
+//     * append image at t, b, l or r
+//     * only 100% scale possible
+//     *
+//     * @param $pathImage
+//     * @param $pathTag
+//     * @param $tagPosition
+//     * @param $pathDest
+//     */
+//    private function _appendToImage($pathImage, $pathTag, $tagPosition, $pathDest)
+//    {
+//        $sizeSrc = getimagesize($pathImage);
+//        $widthSrc = $sizeSrc[0];
+//        $heightSrc = $sizeSrc[1];
+//        $sizeTag = getimagesize($pathTag);
+//        $widthTag = $sizeTag[0];
+//        $heightTag = $sizeTag[1];
+//
+//        // load images
+//        $imImage = UtilImage::loadImage($pathImage);
+//        $imTag = UtilImage::loadImage($pathTag);
+//
+//
+//        if ($tagPosition == "T" || $tagPosition == "B") {
+//            $scaleTag = (float)$widthSrc / $widthTag;
+//            // W/H of Dest
+//            $newWidth = $widthSrc;
+//            $newHeight = $heightSrc + $heightTag * $scaleTag;
+//
+//            $dstXImage = 0;
+//            $dstYImage = $tagPosition == 'T' ? $heightTag * $scaleTag : 0;
+//
+//            $dstXTag = 0;
+//            $dstYTag = $tagPosition == 'T' ? 0 : $heightSrc;
+//        } else { // L or R
+//            $scaleTag = (float)$heightSrc / $heightTag;
+//            $newWidth = $widthSrc + $widthTag * $scaleTag;
+//            $newHeight = $heightSrc;
+//
+//            $dstXImage = $tagPosition == 'L' ? $widthTag * $scaleTag : 0;
+//            $dstYImage = 0;
+//
+//            $dstXTag = $tagPosition == 'L' ? 0 : $widthSrc;
+//            $dstYTag = 0;
+//        }
+//
+//        // create empty image
+//        $imDest = imagecreatetruecolor($newWidth, $newHeight);
+//#	imagealphablending( $imDest, true);
+//#	imagesavealpha( $imDest, true);
+//        imagefill($imDest, 0, 0, 0xff0000);
+//
+//        // copy image (not resized)
+//        imagecopy($imDest, $imImage, $dstXImage, $dstYImage, 0, 0, $widthSrc, $heightSrc);
+//        // copy tag (resampled)
+//        imagecopyresampled($imDest, $imTag, $dstXTag, $dstYTag, 0, 0, $widthTag * $scaleTag, $heightTag * $scaleTag, $widthTag, $heightTag);
+//        UtilImage::saveImage($imDest, $pathDest);
+//    }
 
 
     /**
@@ -80,9 +83,9 @@ class ImageTagger
      * ML MC MR
      * BL BC BR
      *
-     *     AT
-     * AL      AR
-     *     AB
+     * DERECATED     AT
+     * DERECATED AL      AR
+     * DERECATED     AB
      *
      *
      * @param $pathImage
@@ -92,26 +95,46 @@ class ImageTagger
      * @param $pathDest
      * @throws \Exception
      */
-    public function tagImage(string $pathImage, string $pathTag, string $position, $inScale, string $pathDest)
+    public static function tagImage(string $pathImage, string $pathTag, string $position, $inScale, string $pathDest)
+    {
+        $imDest = UtilImage::loadImage($pathImage);
+        $imTag = UtilImage::loadImage($pathTag);
+
+        self::tagImage2018($imDest, $imTag, $position, $inScale, 0);
+        UtilImage::saveImage($imDest, $pathDest);
+    }
+
+
+    /**
+     * 05/2018 this version uses gd resources
+     *
+     * @param $imDest
+     * @param $imTag
+     * @param $position
+     * @param $inScale
+     * @throws \Exception
+     * @throws \WhyooOs\Util\AssertException
+     */
+    public static function tagImage2018($imDest, $imTag, $position, $inScale, $paddingPercent)
     {
 
         // 0) check if we know the position parameter
-        if (!in_array($position, self::ALLOWED_POSITIONS)) {
-            throw new \Exception("position $position not allowed. allowed are: " . implode(', ', self::ALLOWED_POSITIONS));
-        }
+        UtilAssert::assertInArray($position, self::ALLOWED_POSITIONS);
 
-#file_get_contents( $pathImage);
-//var_dump( $pathImage); die( "ppppppppppppppppppppp");
-//$pathTag = $pathImage;
         // 0.5) get image sizes
-        $sizeSrc = getimagesize($pathImage);
-        $widthSrc = $sizeSrc[0];
-        $heightSrc = $sizeSrc[1];
-        $sizeTag = getimagesize($pathTag);
-//        dump(func_get_args()); die();
+        $widthSrc = imagesx($imDest);
+        $heightSrc = imagesy($imDest);
+        $widthTag = imagesx($imTag);
+        $heightTag = imagesy($imTag);
 
-        $widthTag = $sizeTag[0];
-        $heightTag = $sizeTag[1];
+
+
+
+        // 0.6) paddingInPercent --> paddingPx
+        $referenceForRelativeSize = min($widthSrc, $heightSrc);
+        $paddingPx = $referenceForRelativeSize * $paddingPercent / 100.0;
+
+        //UtilDebug::dd('jksdfhjkshfjksdhfkahsfkj');
 
 
         // 1) decise mode: scaleByWidth or scaleByHeight --> scaleFatcor
@@ -131,9 +154,9 @@ class ImageTagger
             } else {
                 $mode = 'W';
             }
-        } elseif ($position[0] == "A") {
-            $this->_appendToImage($pathImage, $pathTag, $position[1], $pathDest);
-            return;
+//        } elseif ($position[0] == "A") {
+//            $this->_appendToImage($pathImage, $pathTag, $position[1], $pathDest);
+//            return;
         } else {
             throw new \Exception("unknown position $position");
         }
@@ -161,8 +184,6 @@ class ImageTagger
 
 
         // 3) imcopy to src: left/right/centerb .. scaleFactor * $inScale
-        $imDest = UtilImage::loadImage($pathImage);
-        $imTag = UtilImage::loadImage($pathTag);
 
         // dstX
         if ($leftCenterRight == 'L') {
@@ -182,6 +203,23 @@ class ImageTagger
             $dstY = ($heightSrc - $newH) / 2; // FIXME?
         }
 
+
+        // UtilDebug::dd($paddingPx);
+
+        // padding X
+        if( $leftCenterRight == 'L') {
+            $dstX += $paddingPx;
+        } elseif($leftCenterRight == 'R') {
+            $dstX -= $paddingPx;
+        }
+
+        // padding Y
+        if( $topMiddleBottom == 'T') {
+            $dstY += $paddingPx;
+        } elseif($topMiddleBottom == 'B') {
+            $dstY -= $paddingPx;
+        }
+
         // rounding
         $dstX = round($dstX);
         $dstY = round($dstY);
@@ -191,10 +229,6 @@ class ImageTagger
         imagealphablending($imDest, true);
         imagesavealpha($imDest, true);
         imagecopyresampled($imDest, $imTag, $dstX, $dstY, 0, 0, $newW, $newH, $widthTag, $heightTag);
-//print( "imagecopyresampled( $dstX , $dstY , 0,0 , $newW, $newH , $widthTag, $heightTag);\n");
-        // return img.
-        #print "save to $pathDest\n";
-        UtilImage::saveImage($imDest, $pathDest);
     }
 
 }
