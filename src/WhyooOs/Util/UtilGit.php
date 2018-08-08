eaebb786f2069330a814670e5fdd26f795b081c6
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2018-08-08 18:46:44 +0200

    UtilGit::getRevisionSummary(), UtilHtml::closeOpenTags() added

--- src/WhyooOs/Util/UtilGit.php
+++ src/WhyooOs/Util/UtilGit.php
@@ -9,20 +9,36 @@ namespace WhyooOs\Util;
 
 class UtilGit
 {
+
+    /**
+     * @param $path
+     * @param $cmd
+     * @param null $output
+     * @return string
+     */
+    private static function _git($path, $cmd, &$output=null)
+    {
+        $paramPath = $path ? " -C $path " : '';
+        $fullCommand = "git $paramPath --no-pager $cmd";
+
+        return exec($fullCommand, $output);
+    }
+
+
     /**
      * legacy
      */
-    public static function version(string $path=null)
+    public static function version(string $path = null)
     {
-        $paramPath = $path ? " -C $path " : '';
-        exec("git $paramPath describe --always", $version_mini_hash);
-        exec("git $paramPath rev-list HEAD | wc -l", $version_number);
-        exec("git $paramPath log -1", $line);
+        
+        self::_git($path, " describe --always", $version_mini_hash);
+        self::_git($path, " rev-list HEAD | wc -l", $version_number);
+        self::_git($path, " log -1", $line);
         $version['short'] = "v1." . trim($version_number[0]) . "." . $version_mini_hash[0]; // ?? v1 ?? remove?
         $version['full'] = "v1." . trim($version_number[0]) . ".$version_mini_hash[0] (" . str_replace('commit ', '', $line[0]) . ")";; // ?? v1 ?? remove?
         // TODO: 2 functions for short and full hash (used for cache busting)
-        $version['shortHash'] = exec("git $paramPath rev-parse --short HEAD");
-        $version['fullHash'] = exec("git $paramPath rev-parse HEAD");
+        $version['shortHash'] = self::_git($path, " rev-parse --short HEAD");
+        $version['fullHash'] = self::_git($path, " rev-parse HEAD");
 
         return $version;
     }
@@ -30,19 +46,17 @@ class UtilGit
     /**
      * used in shipping for "version number"
      */
-    public static function getDate(string $path=null)
+    public static function getDate(string $path = null)
     {
-        $paramPath = $path ? " -C $path " : '';
-        return exec("git $paramPath log -1 --format=%cd --date=short");
+        return self::_git($path, " log -1 --format=%cd --date=short");
     }
 
     /**
      * used in shipping for "version number"
      */
-    public static function getDateRelative(string $path=null)
+    public static function getDateRelative(string $path = null)
     {
-        $paramPath = $path ? " -C $path " : '';
-        return exec("git $paramPath log -1 --format=%cd --date=relative");
+        return self::_git($path, " log -1 --format=%cd --date=relative");
     }
 
     /**
@@ -50,10 +64,85 @@ class UtilGit
      * ideal for cache-buster
      * @return string
      */
-    public static function getShortHash(string $path=null)
+    public static function getShortHash(string $path = null)
     {
-        $paramPath = $path ? " -C $path " : '';
-        return exec("git $paramPath rev-parse --short HEAD");
+        return self::_git($path, " rev-parse --short HEAD");
+    }
+
+    /**
+     * 08/2018 used by marketer for maintenance script
+     *
+     * @param string|null $path
+     * @return array
+     */
+    public static function getRevisionSummary(string $path = null)
+    {
+//    {
+//        "id": "2cd36d9",
+//        "author": "Marc Christenfeldt",
+//        "message": "settings.timezone fix",
+//        "date": "Mon Jan 2 02:49:56 2017 +0100",
+//        "branch": "develop"
+//    }
+
+
+//git_commit_id=$( git --git-dir $GIT_DIR log --oneline --no-merges -1 | cut -d ' ' -f1 )
+//git_commit_author=$( git --git-dir $GIT_DIR   show -s --format='%an' $git_commit_id )
+//git_commit_date=$( git --git-dir $GIT_DIR   show -s --format='%ad' $git_commit_id )
+//git_commit_message=$( git --git-dir $GIT_DIR  )
+//git_branch=$( git --git-dir $GIT_DIR rev-parse --abbrev-ref HEAD )
+
+        return [
+            "dateTime" => self::getDateTime($path),
+            "author" => self::getAuthor($path),
+            "message" => self::getMessage($path),
+            "branch" => self::getBranch($path),
+            "id" => self::getShortHash($path),
+        ];
+    }
+
+    /**
+     * 08/2018
+     *
+     * @param string|null $path
+     * @return string
+     */
+    public static function getAuthor(string $path = null)
+    {
+        return self::_git($path, "   show -s --format='%an' ");
+    }
+
+    /**
+     * 08/2018
+     *
+     * @param string|null $path
+     * @return string
+     */
+    private static function getMessage(string $path = null)
+    {
+        return self::_git($path, 'log --oneline --no-merges -1 | cut -d " " -f2- | sed "s/\'//g"  ');
+    }
+
+    /**
+     * 08/2018
+     *
+     * @param string|null $path
+     * @return string
+     */
+    private static function getBranch(string $path = null)
+    {
+        return self::_git($path, 'rev-parse --abbrev-ref HEAD');
+    }
+
+    /**
+     * 08/2018
+     *
+     * @param string|null $path
+     * @return string
+     */
+    private static function getDateTime(string $path = null)
+    {
+        return self::_git($path, ' show -s --format=%ad ');
     }
 }
 
--- src/WhyooOs/Util/UtilHtml.php
+++ src/WhyooOs/Util/UtilHtml.php
@@ -3,8 +3,11 @@
 namespace WhyooOs\Util;
 
 
+use tidy;
+
 class UtilHtml
 {
+
 
     /**
      * 09/2017 ebaygen - used for emails
@@ -55,9 +58,200 @@ class UtilHtml
     public static function unreplaceHtmlTags($text)
     {
         return preg_replace_callback(
-            '#{{{{{{{{{{(.*?)}}}}}}}}}}#', function ($m) {
+            '#\{\{\{\{\{\{\{\{\{\{(.*?)\}\}\}\}\}\}\}\}\}\}#', function ($m) {
             return base64_decode($m[1]);
         }, $text);
+    }
+
+
+    /**
+     * adds <a> tags for urls in text
+     * also (optionally) trims the text to $maxLength characters (html tags are ignored)
+     * if text was truncated, self::$textWasTruncated is set to true, false otherwise
+     *
+     * 08/2018 moved from WallPostEnhancer to here, added $maxLength,
+     * 08/2018 renamed from linkUrls to linkifyAndTruncateText
+     *
+     * @param string $text
+     * @param int $maxLength
+     * @return array [htmlWithLinksClickable, $textWasTruncated)
+     */
+    public static function linkifyAndTruncateText(string $text, int $maxLength = 0)
+    {
+        // 1) replace html tags to avoid replacements inside of html tags
+        $text = self::replaceHtmlTags($text);
+
+        // ---- if we need to trim we do it now.
+        $bNeedCut = false;
+        if ($maxLength) {
+            $text = preg_replace('!\s+!', ' ', $text);
+            $parts = explode(' ', $text);
+            $textLength = 0;
+            foreach ($parts as $idxWord => $part) {
+
+                # remove encoded html tags because we do not count their text-length
+                $part = preg_replace('#\{\{\{\{\{\{\{\{\{\{(.*?)\}\}\}\}\}\}\}\}\}}#', '', $part);
+                $partLength = mb_strlen($part);
+
+                if ($partLength > 0) {
+                    $textLength += $partLength;
+
+                    # echo "xxx $textLength\n";
+                    if ($textLength > $maxLength) {
+                        $textLength -= mb_strlen($part);
+                        # echo "---break\n";
+                        $bNeedCut = true;
+                        break;
+                    }
+                    $textLength += 1;
+                }
+            }
+            // ---- if needed: do actual cut and close open Tags
+            if ($bNeedCut) {
+#                UtilDebug::d(self::unreplaceHtmlTags($parts[0]));
+                $text = implode(' ', array_slice($parts, 0, $idxWord));
+            }
+        }
+
+        // 2) convert-plain-text-urls-into-html-hyperlink (source: http://stackoverflow.com/questions/1960461/convert-plain-text-urls-into-html-hyperlinks-in-php)
+        $text = preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\{\s])?)?)@', '<a class="wall-link" href="$1" target="_blank">$1</a>', $text);
+
+
+
+
+        // 3) replace back the html tags
+        $text = self::unreplaceHtmlTags($text);
+
+
+        // ---- close open tags
+        if ($bNeedCut) {
+            $text = self::closeOpenTags($text);
+        }
+
+        return [$text, $bNeedCut];
+    }
+
+
+    /**
+     * 08/2018
+     * source: https://stackoverflow.com/a/3810341/2848530
+     * uses tidy http://php.net/manual/en/book.tidy.php
+     *
+     * @param string $text
+     * @return string cleaned html
+     */
+    private static function closeOpenTags(string $text)
+    {
+//  "indent-spaces" => 2
+//  "wrap" => 68
+//  "tab-size" => 8
+//  "char-encoding" => 1
+//  "input-encoding" => 3
+//  "output-encoding" => 1
+//  "newline" => 0
+//  "doctype-mode" => 1
+//  "doctype" => ""
+//  "repeated-attributes" => 1
+//  "alt-text" => ""
+//  "slide-style" => ""
+//  "error-file" => ""
+//  "output-file" => ""
+//  "write-back" => false
+//  "markup" => true
+//  "show-warnings" => true
+//  "quiet" => false
+//  "indent" => 0
+//  "hide-endtags" => false
+//  "input-xml" => false
+//  "output-xml" => false
+//  "output-xhtml" => false
+//  "output-html" => false
+//  "add-xml-decl" => false
+//  "uppercase-tags" => false
+//  "uppercase-attributes" => false
+//  "bare" => false
+//  "clean" => false
+//  "logical-emphasis" => false
+//  "drop-proprietary-attributes" => false
+//  "drop-font-tags" => false
+//  "drop-empty-paras" => true
+//  "fix-bad-comments" => true
+//  "break-before-br" => false
+//  "split" => false
+//  "numeric-entities" => false
+//  "quote-marks" => false
+//  "quote-nbsp" => true
+//  "quote-ampersand" => true
+//  "wrap-attributes" => false
+//  "wrap-script-literals" => false
+//  "wrap-sections" => true
+//  "wrap-asp" => true
+//  "wrap-jste" => true
+//  "wrap-php" => true
+//  "fix-backslash" => true
+//  "indent-attributes" => false
+//  "assume-xml-procins" => false
+//  "add-xml-space" => false
+//  "enclose-text" => false
+//  "enclose-block-text" => false
+//  "keep-time" => false
+//  "word-2000" => false
+//  "tidy-mark" => false
+//  "gnu-emacs" => false
+//  "gnu-emacs-file" => ""
+//  "literal-attributes" => false
+//  "show-body-only" => 0
+//  "fix-uri" => true
+//  "lower-literals" => true
+//  "hide-comments" => false
+//  "indent-cdata" => false
+//  "force-output" => true
+//  "show-errors" => 6
+//  "ascii-chars" => false
+//  "join-classes" => false
+//  "join-styles" => true
+//  "escape-cdata" => false
+//  "language" => ""
+//  "ncr" => true
+//  "output-bom" => 2
+//  "replace-color" => false
+//  "css-prefix" => ""
+//  "new-inline-tags" => ""
+//  "new-blocklevel-tags" => ""
+//  "new-empty-tags" => ""
+//  "new-pre-tags" => ""
+//  "accessibility-check" => 0
+//  "vertical-space" => false
+//  "punctuation-wrap" => false
+//  "merge-divs" => 2
+//  "decorate-inferred-ul" => false
+//  "preserve-entities" => false
+//  "sort-attributes" => 0
+//  "merge-spans" => 2
+//  "anchor-as-name" => true
+
+
+        #return $text;
+
+        $tidy = new Tidy();
+        // workaround about issue with strings like "A <b>B"
+        $wrapperOpen = '<xxx>';
+        $wrapperClose = '</xxx>';
+#        $wrapped = $wrapperOpen . $text . $wrapperClose;
+        $clean = $tidy->repairString($text, [
+//            'output-xml' => true,
+//            'input-xml' => true,
+        'show-body-only' => true,
+            'input-encoding'=> 'utf8',
+            'output-encoding'=> 'utf8',
+            'preserve-entities' => true,
+//            'newline' => 1,
+        ]);
+return $clean;
+//
+//        $withoutWrapper = substr($clean, strlen($wrapperOpen), strlen($clean) - strlen($wrapperOpen) - strlen($wrapperClose));
+//
+//        return $withoutWrapper;
     }
 
 
