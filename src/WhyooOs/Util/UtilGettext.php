f66c9f7c3101ba1a9849fc03a1617f5ca3a8894b
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2021-06-08 11:19:55 +0200

    UtilGettext::getMoFileAsDict() added

--- src/WhyooOs/Util/UtilGettext.php
+++ src/WhyooOs/Util/UtilGettext.php
@@ -3,6 +3,7 @@
 namespace WhyooOs\Util;
 
 use Gettext\Loader\PoLoader;
+use Gettext\Loader\MoLoader;
 
 /**
  * 06/2021 created
@@ -24,6 +25,31 @@ class UtilGettext
     {
         $loader = new PoLoader();
         $translations = $loader->loadFile($pathPoFile);
+
+        $dict = [];
+        /** @var \Gettext\Translation $translation */
+        foreach ($translations->getIterator() as $translation) {
+            $dict[$translation->getOriginal()] = $translation->getTranslation();
+        }
+
+        return $dict;
+    }
+
+
+    /**
+     * this one can be used to transform a .mo file (on-the-fly) to a json dict
+     *
+     * composer require gettext/gettext
+     *
+     * 06/2021 created
+     *
+     * @param string $pathMoFile eg "/app/lang/de.mo"
+     * @return array
+     */
+    public static function getMoFileAsDict(string $pathMoFile): array
+    {
+        $loader = new MoLoader();
+        $translations = $loader->loadFile($pathMoFile);
 
         $dict = [];
         /** @var \Gettext\Translation $translation */
